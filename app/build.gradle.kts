5452ccd6a96052c8421ed452198064de987f690c
Hermanu Widyatama <hermanu.widyatama11@gmail.com>
2024-06-20 03:02:13 +0700

    build auth xml layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -33,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -42,7 +46,18 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.base)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("androidx.core:core-ktx:1.7.0")
+    implementation ("androidx.appcompat:appcompat:1.4.1")
+    implementation ("com.google.android.material:material:1.5.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation ("com.google.firebase:firebase-auth-ktx:21.0.1")
+    implementation ("com.google.android.gms:play-services-auth:20.0.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
 }
--- app/src/main/java/com/example/wirawicara/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/wirawicara/ui/main/MainActivity.kt
@@ -1,21 +1,47 @@
 package com.example.wirawicara.ui.main
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.wirawicara.R
+import com.google.firebase.auth.FirebaseAuth
+import com.example.wirawicara.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        private lateinit var binding: ActivityMainBinding
+        private lateinit var auth: FirebaseAuth;
+
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+            auth = FirebaseAuth.getInstance()
+
+            if(auth.currentUser == null){
+                startActivity(Intent(this, RegisterActivity::class.java))
+                finish()
+            }
+
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            binding.signIn.setOnClickListener {
+                startActivity(Intent(this, RegisterActivity::class.java))
+                finish()
+            }
+            binding.signOut.setOnClickListener {
+                auth.signOut()
+                binding.userDetails.text = updateData()
+            }
         }
-    }
+
+        override fun onResume() {
+            super.onResume()
+            binding.userDetails.text = updateData()
+        }
+
+        private fun updateData(): String{
+            return "Email : ${auth.currentUser?.email}"
+        }
 }
